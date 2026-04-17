a678cb28c85cc49779aed0495abfbc0927cf31c6
bjurd <177191469+bjurd@users.noreply.github.com>
2026-04-17 14:42:42 -0400

    Fix compile error

--- MIDI++/MIDIConnect.hpp
+++ MIDI++/MIDIConnect.hpp
@@ -34,7 +34,7 @@ private:
         {0x48, 0x48}, {0x49, 0x49}, {0x4A, 0x4A}, {0x4E, 0x4E}
     };
 
-    static constexpr size_t MAX_BATCH_INPUTS = 32;
+#define MAX_BATCH_INPUTS 32
     alignas(CACHE_LINE_SIZE) std::array<INPUT, MAX_BATCH_INPUTS> m_batchedInputs;
     alignas(CACHE_LINE_SIZE) std::array<std::array<std::array<INPUT, 10>, 128>, 128> m_noteMapping;
     alignas(CACHE_LINE_SIZE) std::array<std::array<INPUT, 10>, 128> m_sustainMapping;
